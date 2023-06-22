e37d3bb5f46301daa19a3629e8fac05f0398e282
Nanda <nandacoje22@gmail.com>
2023-06-22 11:00:16 +0700

    Menampilkan data tabel users

--- resources/js/components/Sidebar.vue
+++ resources/js/components/Sidebar.vue
@@ -3,17 +3,9 @@
 
 <template>
   <aside
-    class="flex flex-col w-64 h-screen px-5 py-8 overflow-y-auto bg-white border-r rtl:border-r-0 rtl:border-l dark:bg-gray-900 dark:border-gray-700"
+    class="flex flex-col w-64 min-h-screen px-5 py-8 overflow-y-auto bg-white border-r rtl:border-r-0 rtl:border-l dark:bg-gray-900 dark:border-gray-700"
   >
-    <a href="#">
-      <img
-        class="w-auto h-7"
-        src="https://merakiui.com/images/logo.svg"
-        alt=""
-      />
-    </a>
-
-    <div class="flex flex-col justify-between flex-1 mt-6">
+    <div class="flex flex-col justify-between flex-1 mt-2">
       <nav class="-mx-3 space-y-6">
         <div class="space-y-3">
           <!-- Dashboard -->
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,13 +1,15 @@
 <div class="min-h-screen bg-gray-100">
-    @include('layouts.navigation')
+  @include('layouts.admin-navigation')
 
-    <div class="flex space-x-4">
-        <!-- Sidebar vue -->
-        <sidebar />
+  <div class="flex space-x-4">
+    <!-- Sidebar vue -->
+    <sidebar />
 
-        <!-- Page Content -->
-        <main>
-            {{ $slot }}
-        </main>
-    </div>
+    <!-- Page Content -->
+    <main class="flex-1 mt-3">
+      <div class="max-w-6xl mx-auto">
+        {{ $slot }}
+      </div>
+    </main>
+  </div>
 </div>
