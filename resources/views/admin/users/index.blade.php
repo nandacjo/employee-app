12c01d7a58498c5a362320315530958e9c5c5ba9
Nanda <nandacoje22@gmail.com>
2023-06-25 22:47:02 +0700

    Update user menggunakan splade

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,12 +1,10 @@
 <x-admin-layout>
-  <h1 class="text-2xl mb-2 font-medium capitalize">User index</h1>
+  <h1 class="text-2xl font-semibold p-4 capitalize">User index</h1>
   <x-splade-table :for="$users">
     @cell('action', $user)
       <div class="space-x-2">
         <Link href="{{ route('admin.users.edit', $user) }}"
-          class="px-3 py-1 text-white rounded-md bg-green-500 hover:bg-green-700">Edit </Link>
-        <Link href="{{ route('admin.users.destroy', $user) }}"
-          class="px-3 py-1 text-white rounded-md bg-red-500 hover:bg-red-700">Delete </Link>
+          class="px-3 py-1 rounded-md text-green-500 font-semibold hover:text-green-700">Edit </Link>
       </div>
     @endcell
   </x-splade-table>
